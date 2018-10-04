aceeebb95cd7e5cae50e36230150643c28ecf9ad
Marcos Sarges <dev.marcos.sarges@gmail.com>
2018-10-04 13:00:18 -0300

    Repetições

--- Repeticoes/src/repeticoes/Repeticoes.java
+++ Repeticoes/src/repeticoes/Repeticoes.java
@@ -3,8 +3,34 @@ package repeticoes;
 public class Repeticoes {
 
     public static void main(String[] args) {
-        while (true) {
+        /*
+        #VISUALG
+        enquanto <expressão-lógica> faca
+            <seqüência-de-comandos>
+        fimenquanto
+         */
+        //enquanto
+        int condicional = 1;
+        while (condicional <= 10) {
+            System.out.println("Estou aprendendo while em JAVA");
+            System.out.println(condicional);
+            condicional = condicional + 1;
         }
+        
+        
+        
+        /*
+        #Visualg
+        para <variável> de <valor-inicial> ate <valor-limite> [passo <incremento>] faca
+           <sequência-de-comandos>
+        fimpara
+        */
+        //para
+        for (int contador = 1; contador <= 10; contador++) {
+            System.out.println("Estou aprendendo for em JAVA");
+            System.out.println(contador);
+        }
+        
     }
 
 }
